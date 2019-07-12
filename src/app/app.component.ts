6f9c27bfc0976d26342440a9247c4e7aca2716e1
Piotr <ptrysinski@gmail.com>
2019-07-13 01:17:06 +0200

    lista zadan 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lista-zadan';
+
+  newTask: string;
+  tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
+
+  add(){
+    this.tasksList.push(this.newTask);
+    this.newTask = '';
+    console.log(this.tasksList)
+  }
+
+  remove(task: string){
+    this.tasksList = this.tasksList.filter(e => e !== task)
+  }
+
+  done(task: string){
+    this.tasksDone.push(task);
+    this.remove(task)
+  }
 }
