3e06a835e3f79c41e118468e51bee38f3a7a0886
Piotr <ptrysinski@gmail.com>
2019-07-16 01:51:19 +0200

    styles and bootstrap added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+
+
   title = 'lista-zadan';
 
   tasksList: Array<string> = [];
   tasksDone: Array<string> = [];
 
-  add(task : string){
+  ngOnInit(): void {
+    this.tasksList = ['Sprzątanie', 'Nauka', 'Podlewanie', 'Zakupy'];
+  }
+
+  add(task: string) {
     this.tasksList.push(task);
   }
 
-  remove(task: string){
+  remove(task: string) {
     this.tasksList = this.tasksList.filter(e => e !== task)
   }
 
-  done(task: string){
+  done(task: string) {
     this.tasksDone.push(task);
     this.remove(task)
   }
